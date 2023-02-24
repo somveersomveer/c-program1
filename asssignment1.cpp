0704ccc26913bd03d491db2c5a73728930200ee0
somveer <126232323+somveersomveer@users.noreply.github.com>
2023-02-24 08:15:28 +0000

    Pending changes exported from your codespace

---
+++ asssignment1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter any two number"<<endl;
+    cin>>a>>b;
+    if(a>b)
+    cout<<"a is a greater number:"<<a<<endl;
+    else
+    cout<<"b is a greater number:"<<b<<endl; 
+
+    return 0;
+}
